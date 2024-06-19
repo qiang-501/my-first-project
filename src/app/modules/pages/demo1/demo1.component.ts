391e88f2353c0a858e8a621f51195a972b37fe1a
jlyu05 <Jiuru.Lyu@bakerhughes.com>
2024-06-19 10:01:11 +0800

    completed the sorting logic

--- src/app/modules/pages/demo1/demo1.component.ts
+++ src/app/modules/pages/demo1/demo1.component.ts
@@ -5,81 +5,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './demo1.component.scss',
 })
 export class Demo1Component implements OnInit {
-  public items: Array<any> = [];
-  public ChartData: any = {
-    datasets: [
-      {
-        data: [10, 20, 30, 60, 90, 180],
-      },
-    ],
-    labels: ['a', 'b', 'c', 'd', 'e', 'f'],
-  };
-  public ChartOptions: any = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
-  public ChartLabels: any = [
-    'Red',
-    'Blue',
-    'Yellow',
-    'Green',
-    'Purple',
-    'Orange',
-  ];
+  public items: {name: string, value: number}[] = [];
+
   // i need set items sort by name, the same first letter display as a>A , meams it should display as aad,afd,and, Abd,Acd,And,Avd ...
   ngOnInit(): void {
-    for (let i = 0; i < 10000; i++) {
-      let randomCapital = '';
-      let randomLowercase = '';
-      for (let j = 0; j < 4; j++) {
-        randomCapital += String.fromCharCode(
-          65 + Math.floor(Math.random() * 26)
-        );
-        randomLowercase += String.fromCharCode(
-          97 + Math.floor(Math.random() * 26)
-        );
-      }
-      this.items.push({
-        name: randomCapital,
-        value: 12,
-      });
-      this.items.push({
-        name: randomLowercase,
-        value: 12,
-      });
+    for (let i = 0; i < 33000; i++) {
+      this.items.push( {name: this.makeName(5), value: Math.random()} );
     }
-    //set sort here
-    let beginDate = Date.now();
-    this.items.sort((a, b) => {
-      const nameA = a.name.toLowerCase();
-      const nameB = b.name.toLowerCase();
-      let index = 0;
-      while (index < nameA.length && index < nameB.length) {
-        const charA = nameA.charAt(index);
-        const charB = nameB.charAt(index);
-        if (charA < charB) {
-          return -1;
-        }
-        if (charA > charB) {
-          return 1;
-        }
-        if (charA == charB) {
-          if (a.name.charAt(index) > b.name.charAt(index)) {
-            return -1;
-          }
-          if (a.name.charAt(index) < b.name.charAt(index)) {
-            return 1;
-          }
-        }
-        index++;
-      }
 
-      return 0;
-    });
-    let endDate = Date.now();
-    console.log(endDate - beginDate);
+    //set sort here: bubble sort
+    // for (let i = 0; i < this.items.length; i++) {
+    //   for (let j = i + 1; j < this.items.length; j++) {
+    //     if (this.compareTo(this.items[i].name, this.items[j].name) < 0) {
+    //       this.swap(this.items, i, j);
+    //     } 
+    //   }
+    // }
+
+    this.bubbleSort(this.items);
+
+
+  }
+
+  bubbleSort(arr: {name: string, value: number}[]): void {
+    let n: number = arr.length;
+    var swapped: boolean;
+    for (let i = 0; i < n-1; i++) {
+      swapped = false;
+      for (let j = 0; j < n-1-i; j++) {
+        if (this.compareTo(arr[j].name, arr[j+1].name) < 0) {
+          this.swap(arr, j, j+1);
+          swapped = true;
+        }
+      }
+      if (swapped === false) break;
+    }
+  }
+
+  /**
+   * Helper function to achieve the sorting logic
+   * @param s1 - the targetting string
+   * @param s2 - the other string
+   * @return 1 - if s1 is preceeding to s2; -1 if s2 is precedding to s1; 0 if s1 and s2 are identical
+   *  by preceeding: in aA-zZ order, 
+   */
+  compareTo(s1: string, s2: string): number {
+    var i = 0;
+    while (i < s1.length && i < s2.length) {
+      if (s1.toLowerCase().charCodeAt(i) < s2.toLowerCase().charCodeAt(i)) return 1;
+      else if (s1.toLowerCase().charCodeAt(i) > s2.toLowerCase().charCodeAt(i)) return -1;
+      else if (s1.charCodeAt(i) < s2.charCodeAt(i)) return -1;
+      else if (s1.charCodeAt(i) > s2.charCodeAt(i)) return 1;
+      i++;
+    }
+    return 0;
+  }
+
+  /**
+   * Helper functin to swap elements of an array
+   * @param array - the targetted array
+   * @param i - index
+   * @param j - index
+   */
+  swap(array: any[], i: number, j: number): void {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  
+  /**
+   * Helper function to generate random string
+   * @param length - length of desired string
+   * @returns random string
+   */
+  makeName(length: number): string {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
   }
 }
